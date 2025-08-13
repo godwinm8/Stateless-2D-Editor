dca6e1ece9826165da8298cf16976dd9ff4c8cd9
godwinm8 <godwinmano909@gmail.com>
2025-08-14 04:54:21 +0530

    Fix: import useEffect in files using it + fix CSS import path

--- src/pages/CanvasPage.jsx
+++ src/pages/CanvasPage.jsx
@@ -1,5 +1,5 @@
-import { useMemo } from "react";
-import React from "react";
+import React,{ useMemo } from "react";
+
 import { v4 as uuid } from "uuid";
 import CanvasEditor from "../components/CanvasEditor";
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,6 @@
 import { v4 as uuid } from "uuid";
 import { useNavigate } from "react-router-dom";
+import { useEffect } from "react";
 
 export default function Home() {
   const nav = useNavigate();
